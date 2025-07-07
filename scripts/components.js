bff729b1a7f558a33ccfa329218924e1ebc3de90
Zack-Fr <59012974+Zack-Fr@users.noreply.github.com>
2025-07-08 01:39:59 +0300

    split backend

--- scripts/components.js
+++
@@ -1,9 +0,0 @@
-function movieCard(movie) {
-return `<div class="movie-card">
-            <img src="https://i.pravatar.cc/150?img=1" alt="movie.title" />
-            <div>
-            <h4>${movie.title}</h4>
-            <p>${movie.cast}</p>
-            </div>
-        </div>`;
-}
